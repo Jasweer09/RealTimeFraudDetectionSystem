92bc969b755eb5d88fb2ba8cd3222dc35880ad7b
JasweerTadikonda <jasweertadikonda@gmail.com>
2025-04-20 23:17:38 -0700

    Completed the setup for model training

---
+++ src/dags/fraud_detection_training_dag.py
@@ -0,0 +1,71 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.exceptions import AirflowException
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+import logging
+
+logger = logging.getLogger(__name__)
+
+default_args = {
+    'owner': 'jasweertadikonda.com',
+    'depends_on_past': False,
+    'start_date':datetime(2025,4,3),
+    #'execution_timeout': timedelta(minutes=120),
+    'max_active_runs': 1,
+}
+
+def _train_model(**context):
+    """Air flow wrapper for training task"""
+    from fraud_detection_training import FraudDetectionTraining
+    try:
+        logger.info('Intializing fraud detection training')
+        trainer = FraudDetectionTraining()
+        model, precision = trainer.train_model()
+        return {
+            'status': 'success',
+            'precision':precision
+        }
+    except Exception as e:
+        logger.error("Training failed: %s", str(e), exc_info=True)
+        raise AirflowException(f'Model training failed: {str(e)}')
+with DAG(
+    'fraud_detection_training',
+    default_args=default_args,
+    description='Fraud detection model training pipeline',
+    schedule_interval='0 3 * * *',
+    catchup = False,
+    tags = ['fraud', 'ML']
+) as dag:
+    validate_environment = BashOperator(
+        task_id='validate_environment',
+        bash_command='''
+        echo "Validation environment"
+        test -f/app/config.yaml &&
+        test -f/app/.env
+        echo "Environment is valid"
+        '''
+    )
+
+    training_task = PythonOperator(
+        task_id='executing_training',
+        python_callable=_train_model,
+        provide_context=True,
+    )
+
+    cleanup_task = BashOperator(
+        task_id='cleanup_resources',
+        bash_command= 'rm -f /app/tmp/*.pkl',
+        trigger_rule='all_done'
+    )
+
+    validate_environment >> training_task >> cleanup_task
+
+    dag.doc_md="""
+        ##Fraud detection training pipeline
+        Daily training of fraud detection model using:
+        -Transaction data from Kafka
+        -XGBoost classifier with precision optimization
+        -MLFlow for experiment tracking
+    """
